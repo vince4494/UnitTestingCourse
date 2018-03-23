23b2042f20bf34f70b4ba12973a0e2d6f009ad9e
Vince Zipparro <vince.zipparro@coyote.com>
2018-03-23 14:25:37 -0500

    Built on FileNameDoesExist test, added an appconfig file that creates a test file, use the TestContext to add output to the logs to confirm certain areas of the test were successful

--- UnitTestingCourse_Tests/FileProcessTests.cs
+++ UnitTestingCourse_Tests/FileProcessTests.cs
@@ -1,21 +1,45 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UnitTestingCourse;
+using System.Configuration;
+using System.IO;
 
 namespace UnitTestingCourse_Tests
 {
     [TestClass]
     public class FileProcessTests
     {
+        private const string BAD_FILE_NAME = @"C:\Windows\Regedit.exe";
+        private string _GoodFileName;
+
+        //allows to write test output that gets logged that can be used as confirmation that certain areas of the test are successful
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
         public void FileNameDoesExist()
         {
             FileProcess fp = new FileProcess();
             bool fromCall;
 
-            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+            SetGoodFileName();
+            TestContext.WriteLine("Creating the file: " + _GoodFileName);
+            File.AppendAllText(_GoodFileName, "Some Text");
+            TestContext.WriteLine("Testing the file: " + _GoodFileName);
+            fromCall = fp.FileExists(_GoodFileName);
+            TestContext.WriteLine("Deleting the file: " + _GoodFileName);
+            File.Delete(_GoodFileName);
 
             Assert.IsTrue(fromCall);
+        }
+
+        public void SetGoodFileName()
+        {
+            _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            if(_GoodFileName.Contains("[AppPath]"))
+            {
+                _GoodFileName = _GoodFileName.Replace("[AppPath]", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+
         }
 
         [TestMethod]
